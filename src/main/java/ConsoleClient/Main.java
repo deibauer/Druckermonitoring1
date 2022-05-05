9a3d8a9f0e24ebc3f679b82ed9843218eb65bc75
deibauer <104979195+deibauer@users.noreply.github.com>
2022-05-05 12:35:11 +0200

    Klassen und Packages hinzugefügt

--- src/main/java/ConsoleClient/Main.java
+++ src/main/java/ConsoleClient/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package ConsoleClient;
 
 public class Main {
     public static void main(String[] args) {
---
+++ src/main/java/ConsoleClient/consoleClient.java
@@ -0,0 +1,4 @@
+package ConsoleClient;
+
+public class consoleClient {
+}
